b4966bafbd8d2fa186980263e5e3a30afda98ec6
rfxct <xbrcvar@gmail.com>
2021-11-01 03:34:14 -0300

    create date transformator

--- src/util/DateUtil.js
+++ src/util/DateUtil.js
@@ -1,3 +1,13 @@
 export const Long = new Intl.DateTimeFormat('pt-BR', {
   timeZone: 'America/Sao_Paulo', timeStyle: 'short', dateStyle: 'medium'
 })
+
+export const isDate = (value) => {
+  const result = new Date(value)
+  return result instanceof Date && !isNaN(result.valueOf())
+}
+
+export const displayDate = (value) => {
+  const result = new Date(value)
+  return isDate(value) ? Long.format(result) : '---'
+}

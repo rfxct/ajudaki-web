dcfa7e8d6150eecb62f12ff3ae9fa9dc2cb0e7d4
marc <rfxct@gmail.com>
2021-11-30 20:39:09 -0300

    rewrite function

--- src/util/GenAvatar.js
+++ src/util/GenAvatar.js
@@ -1,23 +1,22 @@
 export default function genAvatar(full_name = '') {
-  const letters = Array.from('abcdefghijklmnopqrstuvwxyz')
-  // Remove acentos
-  const names = full_name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').trim().split(/\s+/)
+  let hash = 0;
+  for (let i = 0; i < full_name.length; i++) {
+    hash = full_name.charCodeAt(i) + ((hash << 3) - hash)
+  }
 
-  // Pega as iniciais de cada nome
-  const primary = [names[0], names.slice(-1)].map(String).map(s => !!s ? s[0] : '')
-  // Pega a letra do meio de cada nome
-  const secondary = [names[0], names.slice(-1)].map(String).map(s => !!s ? s[Math.floor(s.length / 2)] : '')
+  const color = Math.abs(hash).toString(16).substring(0, 6)
+  const backgroundColor = `#${'000000'.substring(0, 6 - color.length) + color}`
 
-  // Gera um número de acordo com a letra fornecida (busca o índice dele no array letters)
-  const prefixes = [...primary, ...secondary]
-    .map(l => String(letters.indexOf(l.toLowerCase())).padStart(2, 'b')).join('')
-  const hexColor = `#${full_name?.length ? prefixes : 'adb5bd'}`
+  const prefixes = full_name.trim().split(/\s+/)
+    .filter((_, i, a) => i === 0 || i === a.length - 1)
+    .map(s => s[0].toUpperCase())
+    .join('')
 
   return (
     <div className="rounded-circle custom-avatar" style={{
-      backgroundColor: hexColor.substr(0, 7)
+      backgroundColor
     }}>
-      <span>{primary}</span>
+      <span>{prefixes}</span>
     </div >
   )
 }
